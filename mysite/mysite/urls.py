80dacefe31e376758e24ca17f1ae654953f228c3
Memet Koslu <memet@startmail.com>
2018-02-05 16:18:06 +0300

    Home and Budget app added

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,4 +19,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls')),
+    path('budget/', include('budget.urls')),
+    path('home/', include('home.urls')),
 ]
